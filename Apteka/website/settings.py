471ee98598d13ddd3c52f84f0ae24c57fe599988
KonradMaroszek <KonradMaroszek@interia.eu>
2017-11-11 13:34:33 +0100

    added main page , instruction file

--- Apteka/website/settings.py
+++ Apteka/website/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Apteka.urls'
+ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Apteka.wsgi.application'
+WSGI_APPLICATION = 'website.wsgi.application'
 
 
 # Database
