1767a7e74a0e2d6724876e6b888b0ccfa4cbab50
KonradMaroszek <KonradMaroszek@interia.eu>
2017-11-11 17:01:22 +0100

    updated database

--- Apteka/apteka/models.py
+++ Apteka/apteka/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    amount = models.IntegerField(default=0)
+    product_logo = models.CharField(default = 'null', max_length=300)
+    description = models.TextField(default = "", max_length=9999)
+
+    def __str__(self):
+        return self.name
