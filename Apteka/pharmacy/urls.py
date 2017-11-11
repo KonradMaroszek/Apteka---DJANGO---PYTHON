4a7a3eb4e26a6bc89e964b587b534f9a35fc9052
KonradMaroszek <KonradMaroszek@interia.eu>
2017-11-12 00:03:47 +0100

    added new templates

--- Apteka/pharmacy/urls.py
+++ Apteka/pharmacy/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'pharmacy'
+
 urlpatterns = [
     #/pharmacy/
     url(r'^$', views.index, name='index'),
@@ -8,4 +10,7 @@ urlpatterns = [
     url(r'^products/$', views.products, name='products'),
     #pharmacy/products/1
     url(r'^products/(?P<product_id>[0-9]+)/$', views.product_detail, name='product_detail'),
+    #pharmacy/add_to_basket
+    url(r'^add_to_basket/$', views.add_to_basket, name='add_to_basket'),
+
 ]
--- Apteka/pharmacy/views.py
+++ Apteka/pharmacy/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
     return HttpResponse("<h1>This is APteka</h1>")
 
 def products(request):
     all_products = Product.objects.all()
-    context = {'all_products' : all_products}
-    return render(request, 'pharmacy/products.html', context)
+    return render(request, 'pharmacy/products.html', {'all_products' : all_products})
 
 def product_detail(request, product_id):
-    return HttpResponse("<h2>Details for product id:"  + str(product_id) +  "</h2>")
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'pharmacy/product_detail.html', {'product': product})
+
+def add_to_basket(request):
+    try:
+        selected_product = get_object_or_404(Product,pk=request.POST['add_to_basket'])
+    except (KeyError, ObjectDoesNotExist):
+        return render(request, 'pharmacy/products.html', {'error_message' : "Cannot add this product to basket"})
+    else:
+        return render(request, 'pharmacy/index.html', {'message': "Added new product"})
