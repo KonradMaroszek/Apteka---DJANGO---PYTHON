c0a4b47fd217e474c40a1f838fc1354edf7c0ca9
KonradMaroszek <KonradMaroszek@interia.eu>
2017-11-11 17:57:04 +0100

    first page

--- Apteka/apteka/urls.py
+++ Apteka/apteka/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    #/apteka/
+    url(r'^$', views.index, name='index'),
+    #/apteka/products
+    url(r'^products/$', views.products, name='products'),
+    #apteka/products/1
+    url(r'^products/(?P<product_id>[0-9]+)/$', views.product_detail, name='product_detail'),
 ]
--- Apteka/apteka/views.py
+++ Apteka/apteka/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
 
 def index(request):
     return HttpResponse("<h1>This is APteka</h1>")
+
+def products(request):
+    all_products = Product.objects.all()
+    context = {'all_products' : all_products}
+    return render(request, 'apteka/products.html', context)
+
+def product_detail(request, product_id):
+    return HttpResponse("<h2>Details for product id:"  + str(product_id) +  "</h2>")
